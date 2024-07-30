602ff15f465e4b52713d705a6173490b9ee85c95
Ugo Chinemerem <ugofranklin22@gmail.com>
2024-07-30 02:12:29 +0100

    add:options

--- src/routes/action.routes.ts
+++ src/routes/action.routes.ts
@@ -2,10 +2,11 @@
 
 import express from "express";
 import ActionController from "../controllers/action.controllers";
-const { getAction } = new ActionController();
+const { getAction, postAction } = new ActionController();
 
 const router = express.Router();
 
 router.get("/:name", getAction);
+router.options("/:name", getAction);
 
 export default router;
