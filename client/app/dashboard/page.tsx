29d9a7f33d6e760cf387580554d732c72b573233
Ugo Chinemerem <ugofranklin22@gmail.com>
2024-08-16 00:40:21 +0100

    refactor:auth

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
-import React from "react";
+import { useEffect, useRef, useState } from "react";
 import Grid from "@mui/material/Grid";
 import CircularProgress from "@mui/material/CircularProgress";
 import { gridSpacing } from "../components/store/constant";
@@ -12,52 +11,59 @@ import Customers from "../components/cards/customers/Customers";
 import Revenue from "../components/cards/revenue/Revenue";
 import Chart from "../components/chart/Chart";
 import TransactionCard from "../components/cards/Transaction/TransactionCard";
+import { useRouter } from "next/navigation";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const Page = () => {
   const [isLoading, setLoading] = useState(true);
+  const { connected } = useWallet();
+  const router = useRouter();
 
   useEffect(() => {
-    setLoading(false);
-  }, []);
+    if (!connected) {
+      router.push("/");
+    }
+  }, [connected, router]);
 
+  if (!connected) {
+    toast.error("Wallet not connected! Redirecting to the home page...");
+  }
   return (
     <>
-      {isLoading ? (
-        <CircularProgress />
-      ) : (
-        <Grid className="mt-2" container spacing={gridSpacing}>
-          <Grid item xs={12} sm={6} md={3}>
-            <Balance />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Orders />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Sales />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Earning />
-          </Grid>
+      <Grid className="mt-2" container spacing={gridSpacing}>
+        <Grid item xs={12} sm={6} md={3}>
+          <Balance />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Orders />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Sales />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Earning />
+        </Grid>
 
-          <Grid item xs={12}>
-            <Grid container spacing={gridSpacing}>
-              <Grid item xs={12} md={8}>
-                <Chart />
-              </Grid>
-              <Grid item xs={12} md={4}>
-                <Grid container direction="row" spacing={2}>
-                  <Grid item xs={6}>
-                    <Customers />
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Revenue />
-                  </Grid>
+        <Grid item xs={12}>
+          <Grid container spacing={gridSpacing}>
+            <Grid item xs={12} md={8}>
+              <Chart />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Grid container direction="row" spacing={2}>
+                <Grid item xs={6}>
+                  <Customers />
                 </Grid>
-                <TransactionCard />
+                <Grid item xs={6}>
+                  <Revenue />
+                </Grid>
               </Grid>
+              <TransactionCard />
             </Grid>
           </Grid>
         </Grid>
-      )}
+      </Grid>
     </>
   );
 };
