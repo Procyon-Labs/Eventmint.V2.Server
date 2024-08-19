6bf803dc10e9e0ee5563d1070d849b8f1eebd94a
Ugo Chinemerem <ugofranklin22@gmail.com>
2024-08-19 21:02:32 +0100

    add:profile-page

--- client/app/dashboard/profile/page.tsx
+++ client/app/dashboard/profile/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Image from "next/image";
 import { Button } from "@/component/button";
+import { useWallet } from "@solana/wallet-adapter-react";
+
 const Page = () => {
   return (
     <section className="flex gap-4 mt-2">
@@ -21,9 +23,33 @@ const Page = () => {
       </div>
       <div className="flex flex-col w-4/6 items-start gap-4 p-2 px-4 self-stretch justify-center rounded-[24px] bg-[rgba(25,29,35,0.5)]">
         <div>
-          <h2></h2>
-          <p></p>
+          <h2 className="text-[#E7EAEE]">#019900</h2>
+          <p className="text-[#64748B]">User ID</p>
         </div>
+        <div>
+          <h2 className="text-[#E7EAEE]">Luke</h2>
+          <p className="text-[#64748B]">First Name</p>
+        </div>
+        <div>
+          <h2 className="text-[#E7EAEE]">Skywalker</h2>
+          <p className="text-[#64748B]">Last Name</p>
+        </div>
+        <div>
+          <h2 className="text-[#E7EAEE]">luke.skywalker@mail.com</h2>
+          <p className="text-[#64748B]">Email address</p>
+        </div>
+        <div>
+          <h2 className="text-[#E7EAEE]">
+            Keeping the universe safe from Darth Vader is a personal hobby 😁☺️
+          </h2>
+          <p className="text-[#64748B]">Bio</p>
+        </div>
+        <Button
+          type="submit"
+          label="Edit Profile"
+          customClassName="mt-4 text-body-xxs font-open-sans bg-gradient-to-b-custom rounded-[12px] mx-auto hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          size="moreMedium"
+        />
       </div>
     </section>
   );
