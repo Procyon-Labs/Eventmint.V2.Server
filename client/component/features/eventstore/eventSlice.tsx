fd2079ac8927f8de70859d735d98abc276aa844d
Ugo Chinemerem <ugofranklin22@gmail.com>
2024-08-22 11:41:27 +0100

    add:eventslice

---
+++ client/component/features/eventstore/eventSlice.tsx
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  events: [] as any[],
+};
+
+const eventSlice = createSlice({
+  name: "event",
+  initialState,
+  reducers: {
+    addEvent: (state, action) => {
+      state.events.push({
+        ...action.payload.event,
+        blink: action.payload.blink,
+      });
+    },
+    clearEvents: (state) => {
+      state.events = [];
+    },
+  },
+});
+export const { addEvent, clearEvents } = eventSlice.actions;
+export default eventSlice.reducer;
