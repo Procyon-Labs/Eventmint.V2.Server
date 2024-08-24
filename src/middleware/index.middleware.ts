13a2a74fded6464e13a3bef5236e84182820523a
Ugo Chinemerem <ugofranklin22@gmail.com>
2024-08-24 01:00:13 +0100

    app:cors

--- src/middleware/index.middleware.ts
+++ src/middleware/index.middleware.ts
@@ -1,15 +1,21 @@
-import { Application, json, urlencoded } from 'express';
-import * as dotenv from 'dotenv';
+import { Application, json, urlencoded } from "express";
+import * as dotenv from "dotenv";
 dotenv.config();
-import morgan from 'morgan';
-import cors from 'cors';
-import helmet from 'helmet';
-import asyncError from './errors.middlewares';
-import indexRoutes from '../routes/index.routes';
+import morgan from "morgan";
+import cors from "cors";
+import helmet from "helmet";
+import asyncError from "./errors.middlewares";
+import indexRoutes from "../routes/index.routes";
 
 export default (app: Application) => {
-  app.use(morgan('combined'));
-  app.use(cors());
+  app.use(morgan("combined"));
+  app.use(
+    cors({
+      origin: `*`,
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      credentials: true,
+    })
+  );
   app.use(json());
   app.use(helmet());
   app.use(urlencoded({ extended: true }));
