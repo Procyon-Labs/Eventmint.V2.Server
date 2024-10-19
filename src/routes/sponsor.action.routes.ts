55b1a915bcb4f107b6c184660ee4b6b1d48dffc2
Ugo Chinemerem <ugofranklin22@gmail.com>
2024-10-19 23:25:59 +0100

    fix:route_changes

--- src/routes/sponsor.action.routes.ts
+++ src/routes/sponsor.action.routes.ts
@@ -1,12 +1,20 @@
 // src/routes/action.routes.ts
-
+import { createActionHeaders } from '@solana/actions';
 import express from 'express';
+import { Request, Response } from 'express';
+
 import ActionController from '../controllers/action.controllers';
 const { getAction, postAction } = new ActionController();
 
 const router = express.Router();
 
-router.get('/:name', getAction);
-router.options('/:name', getAction);
-router.post('/:name', postAction);
+router.get('/:keymessage', getAction);
+//options action
+router.options('/:keymessage', (_req: Request, res: Response) => {
+  const headers = createActionHeaders();
+  res.set(headers);
+  return res.json();
+});
+
+router.post('/:keymessage', postAction);
 export default router;
