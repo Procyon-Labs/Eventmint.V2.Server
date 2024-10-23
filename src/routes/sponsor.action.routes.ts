4630335b26d1a652997a5c237472454fa7cc0707
Adaobi Chuks-Aginam <chuksaginamada@yahoo.com>
2024-10-23 07:43:31 +0100

    Update cors headers for option request

--- src/routes/sponsor.action.routes.ts
+++ src/routes/sponsor.action.routes.ts
@@ -1,5 +1,5 @@
 // src/routes/action.routes.ts
-import { createActionHeaders } from '@solana/actions';
+import { ACTIONS_CORS_HEADERS, createActionHeaders } from '@solana/actions';
 import express from 'express';
 import { Request, Response } from 'express';
 
@@ -10,7 +10,20 @@ const router = express.Router();
 
 router.get('/:keymessage', getAction);
 //options action
-router.options('/:keymessage', getAction);
+router.options('/:keymessage', (_req: Request, res: Response) => {
+  res.set(createActionHeaders({
+    chainId: "devnet",
+    actionVersion: "2.2.3"
+  }));
+  res.set({
+    ...ACTIONS_CORS_HEADERS,
+    "X-Action-Version": "2.1.3",
+    // "X-Blockchain-Ids": "solana:EtWTRABZaYq6iMfeYKouRu166VU2xqa1"
+    "X-Blockchain-Ids": "solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp"
+  });
+
+  return res.json();
+});
 
 router.post('/:keymessage', postAction);
 export default router;
