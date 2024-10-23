586c22e18a682db2a0da0d876758eaa633ab9265
Adaobi Chuks-Aginam <chuksaginamada@yahoo.com>
2024-10-23 07:47:09 +0100

    update

--- src/routes/sponsor.action.routes.ts
+++ src/routes/sponsor.action.routes.ts
@@ -1,5 +1,5 @@
 // src/routes/action.routes.ts
-import { createActionHeaders } from '@solana/actions';
+import { ACTIONS_CORS_HEADERS, createActionHeaders } from '@solana/actions';
 import express from 'express';
 import { Request, Response } from 'express';
 
@@ -9,10 +9,19 @@ const { getAction, postAction } = new SponsorController();
 const router = express.Router();
 
 router.get('/:keymessage', getAction);
-//options action
+
 router.options('/:keymessage', (_req: Request, res: Response) => {
-  const headers = createActionHeaders();
-  res.set(headers);
+  res.set(createActionHeaders({
+    chainId: "devnet",
+    actionVersion: "2.2.3"
+  }));
+  res.set({
+    ...ACTIONS_CORS_HEADERS,
+    "X-Action-Version": "2.1.3",
+    // "X-Blockchain-Ids": "solana:EtWTRABZaYq6iMfeYKouRu166VU2xqa1"
+    "X-Blockchain-Ids": "solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp"
+  });
+
   return res.json();
 });
 
